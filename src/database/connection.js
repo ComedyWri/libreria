39053b4990e78d58e928a5b3a8b5fd7c61f7accf
Ichiro Hirai <98781960+ComedyWri@users.noreply.github.com>
2023-03-24 13:52:25 -0400

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,7 +5,7 @@ dotenv.config()
 //creando la conexion a la Base de Datos utilizando .dotenv y sequelize.
 
 const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.db_HOST,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     port: process.env.DB_PORT
 });
