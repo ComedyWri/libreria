891cb81a035e3d0ce03fd97adacae65c7909fe2c
Ichiro Hirai <98781960+ComedyWri@users.noreply.github.com>
2023-03-28 09:32:17 -0400

    Update users.controllers.js

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -43,7 +43,7 @@ const loginUser = async(req, res) => {
     //confirmando credenciales
     if (passDecrypt == true && accesUser[0].dataValues.user_email == email){
         const uCookie = uuidv4()
-        res.cookie('session_token', uCookie, {sameSite: 'none', httpOnly: true, secure: true})
+        res.cookie('session_token', uCookie, {sameSite: None, httpOnly: true, secure: true})
         res.status(200).json('Correcto')
     }else{
         res.status[422].json('The credentials are wrong')
