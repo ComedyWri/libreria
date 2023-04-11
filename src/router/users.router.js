e68a1ec6f052de81cbfc64c57fb93646fdbfb760
Ichi-San <98781960+ComedyWri@users.noreply.github.com>
2023-04-11 17:06:45 -0400

    Update users.router.js

--- src/router/users.router.js
+++ src/router/users.router.js
@@ -6,8 +6,10 @@ router.post('/register', registerUser)
 
 router.post('/login', loginUser)
 
-router.post('/logout', (req, res) => {
-    res.clearCookie('session_token')
+router.get('/logout', (req, res) => {
+    res.clearCookie('session_token', {
+    {domain: 'libreria-production.up.railway.app', path: '/', sameSite: 'none', secure: true}
+    })
     res.sendStatus(205)
 })
 
